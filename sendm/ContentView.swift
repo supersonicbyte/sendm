9adcf857584b96d20d25749939f8d934b40ea5e0
Mirza Ucanbarlic <mirza.ucanbarlic@klika.ba>
2021-12-18 04:22:54 +0100

    Initial Commit

---
+++ sendm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sendm
+//
+//  Created by Mirza Ucanbarlic on 18. 12. 2021..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sendm/sendmApp.swift
@@ -0,0 +1,17 @@
+//
+//  sendmApp.swift
+//  sendm
+//
+//  Created by Mirza Ucanbarlic on 18. 12. 2021..
+//
+
+import SwiftUI
+
+@main
+struct sendmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
